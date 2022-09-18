32d9771e7555e62ca54b70c4afe8cfe4d91be299
manish mandal <76776194mm@gmail.com>
2022-09-19 01:00:34 +0530

    update script.js

--- script.js
+++ script.js
@@ -13,10 +13,23 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
-        const { latitude }=position.coords;
+        // console.log(position);
+        const {latitude} =position.coords;
         const {longitude}=position.coords;
-        console.log(`https://www.google.co.in/maps/@${latitude},${longitude}`);
+        console.log(`https://www.google.co.in/maps/@ ${latitude},${longitude}`);
+        const coords=[latitude,longitude];
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        
+        L.marker(coords)
+            .addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+
     },function(){
-        alert('could not get your position');
+        alert('Could not get your position');
     });
 }
