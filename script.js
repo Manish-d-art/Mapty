1cd5faf327e623c60e72b693e19cb484d3c57645
manish mandal <76776194mm@gmail.com>
2022-09-20 19:39:41 +0530

    update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map;
+let map,mapEvent;
 
 
 if(navigator.geolocation){
@@ -25,7 +25,8 @@ if(navigator.geolocation){
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
-        map.on('click',function(mapEvent){
+        map.on('click',function(mapE){
+            mapEvent=mapE;
             form.classList.remove('hidden');
             inputDistance.focus();
             
