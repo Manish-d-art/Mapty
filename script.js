3a5627c94bf7e207f9bae43f84ab74544027d537
manish mandal <76776194mm@gmail.com>
2022-09-20 19:02:54 +0530

    update script.js

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map;
+
 
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
@@ -18,29 +20,35 @@ if(navigator.geolocation){
         const {longitude}=position.coords;
         console.log(`https://www.google.co.in/maps/@ ${latitude},${longitude}`);
         const coords=[latitude,longitude];
-        const map = L.map('map').setView(coords, 13);
+        map = L.map('map').setView(coords, 13);
 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
         map.on('click',function(mapEvent){
-            const {lat,lng}=mapEvent.latlng;
-            L.marker([lat,lng])
-            .addTo(map)
-            .bindPopup(
-                L.popup({
-                    maxWidth:250,
-                    minWidth:100,
-                    autoClose:false,
-                    closeOnClick:false,
-                    className:'running-popup',
-                })
-            )
-            .setPopupContent('Workout')
-            .openPopup();
+            form.classList.remove('hidden');
+            inputDistance.focus();
+            
         })
         
     },function(){
         alert('Could not get your position');
     });
 }
+
+form.addEventListener('submit',function(){
+    const {lat,lng}=mapEvent.latlng;
+                L.marker([lat,lng])
+                .addTo(map)
+                .bindPopup(
+                    L.popup({
+                        maxWidth:250,
+                        minWidth:100,
+                        autoClose:false,
+                        closeOnClick:false,
+                        className:'running-popup',
+                    })
+                )
+                .setPopupContent('Workout')
+                .openPopup();
+})
