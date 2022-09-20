a43bd4589f119067e999c0416a8735afa810ac57
manish mandal <76776194mm@gmail.com>
2022-09-20 20:12:45 +0530

    update script.js

--- script.js
+++ script.js
@@ -55,4 +55,9 @@ form.addEventListener('submit',function(e){
                 )
                 .setPopupContent('Workout')
                 .openPopup();
+});
+
+inputType.addEventListener('change',function(){
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
 })
