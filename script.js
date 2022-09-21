fa7e60d94b94d55c7fccf88c29687bd33e9cbd0a
manish mandal <76776194mm@gmail.com>
2022-09-21 12:19:05 +0530

    update script.js

--- script.js
+++ script.js
@@ -47,18 +47,16 @@ class App{
             L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(this.#map);
-    
-            this.#map.on('click',function(mapE){
-                this.#mapEvent=mapE;
-                form.classList.remove('hidden');
-                inputDistance.focus();
-                
-            })
             
+            //handling click on map
+            this.#map.on('click',this._showForm.bind(this));
+                
     }
 
-    _showForm(){
-
+    _showForm(mapE){
+        this.#mapEvent=mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
     }
 
     _toggleElevationField(){
